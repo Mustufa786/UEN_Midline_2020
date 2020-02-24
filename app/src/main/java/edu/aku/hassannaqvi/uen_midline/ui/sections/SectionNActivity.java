b601f73c9eccb2573be68e2d6f2963106943b8cb
SAJID LATIF <indus1530@gmail.com>
2020-02-24 17:10:21 +0500

    N103c skip to N106 by Hamza

--- app/src/main/java/edu/aku/hassannaqvi/uen_midline/ui/sections/SectionNActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_midline/ui/sections/SectionNActivity.java
@@ -2,6 +2,7 @@ package edu.aku.hassannaqvi.uen_midline.ui.sections;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -37,8 +38,14 @@ public class SectionNActivity extends AppCompatActivity {
     private void setUIComponent() {
 
         bi.n103.setOnCheckedChangeListener(((radioGroup, i) -> {
-            if (i != bi.n103c.getId()) {
+            if (i == bi.n103c.getId()) {
+                bi.fldGrpCVn104.setVisibility(View.VISIBLE);
+                bi.fldGrpCVn105.setVisibility(View.VISIBLE);
+            } else {
+                bi.fldGrpCVn104.setVisibility(View.GONE);
+                bi.fldGrpCVn105.setVisibility(View.GONE);
                 Clear.clearAllFields(bi.fldGrpCVn104);
+                Clear.clearAllFields(bi.fldGrpCVn105);
             }
 
         }));
