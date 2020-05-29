fdd74c4e77b847d6c654900227de791cd8f5120c
muhammad.farooqui <waqarfarooqui.12@gmail.com>
2020-05-29 12:10:58 +0500

    correct skip of H132.

--- app/src/main/java/edu/aku/hassannaqvi/uen_midline/ui/sections/SectionH102Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_midline/ui/sections/SectionH102Activity.java
@@ -134,11 +134,11 @@ public class SectionH102Activity extends AppCompatActivity {
         //h132
         bi.h132.setOnCheckedChangeListener((group, checkedId) -> {
 
-            if (checkedId == bi.h132a.getId()) {
-                bi.fldGrpCVh133.setVisibility(View.VISIBLE);
+            if (checkedId == bi.h132b.getId()) {
+                bi.fldGrpCVh1321.setVisibility(View.VISIBLE);
             } else {
-                Clear.clearAllFields(bi.fldGrpCVh133);
-                bi.fldGrpCVh133.setVisibility(View.GONE);
+                Clear.clearAllFields(bi.fldGrpCVh1321);
+                bi.fldGrpCVh1321.setVisibility(View.GONE);
             }
         });
 
